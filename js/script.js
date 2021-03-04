85e1b6c7b3564bfaa0f1f5748d1bc319a11b266c
Tommaso P <tommaso.plinio@gmail.com>
2021-03-04 11:57:45 +0100

    Snack 9

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un
+// numero. Il programma stampa la somma di tutti i numeri
+// inseriti. Esegui questo programma in due versioni, con il for
+// e con il while.
+var somma = 0;
+
+for (i=0; i<5; i++) {
+  var numeroUtente = parseInt(prompt("Inserisci un numero"));
+  somma += numeroUtente;
+}
+
+console.log(somma);
